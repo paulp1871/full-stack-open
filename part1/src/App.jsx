018ffb2ce2622be9f8e4527bee184848846c1d5d
Paul Pham <paulpham1871@gmail.com>
2025-12-22 18:03:24 -0700

    Finished part 1a Introduction to React

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,23 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
+  )
+}
+
+
+const App = () => {
+  const name = 'Peter'
+  const age = '10'
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10}/>
+      <Hello name={name} age={age}/>
+    </>
   )
 }
 
