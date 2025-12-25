479f5a3ac51e027fe87f93422933890dcef20018
Paul Pham <paulpham1871@gmail.com>
2025-12-25 16:18:01 -0700

    Revert "Completed exercise 1.3"
    This reverts commit 7209163082aee645c9a4c311208d7c3a21ac0ed4.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,21 +8,35 @@ const Header = ({course}) => {
   );
 }
 
-const Part = ({name, exercises}) => {
+const Part = ({name, amount}) => {
+  console.log(name, amount);
+
   return (
     <div>
       <p>
-        {name} {exercises}
+        {name} {amount}
       </p>
     </div>
   );
 }
 
-const Content = (parts) => {
-  console.log(parts);
+const Content = ({part1, part2, part3}) => {
+  console.log(part1, part2, part3);
+  
   return (
     <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises}/>
+      <Part 
+        name={part1.name}
+        amount={part1.amount}
+      />
+      <Part 
+        name={part2.name}
+        amount={part2.amount}
+      />
+      <Part 
+        name={part3.name}
+        amount={part3.amount}
+      />
     </div>
   );
 }
@@ -45,24 +59,22 @@ const Total = ({exercises}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={[part1, part2, part3]}/>
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]}/>
+      <Content 
+        part1={{name: part1, amount: exercises1}}
+        part2={{name: part2, amount: exercises2}}
+        part3={{name: part3, amount: exercises3}}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]}/>
     </div>
   )
 }
