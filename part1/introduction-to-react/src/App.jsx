fc6be590f0ebdf29e03a0369d2775bde807d1e79
Paul Pham <paulpham1871@gmail.com>
2025-12-28 00:50:17 -0700

    Started reading part 1c

--- part1/introduction-to-react/src/App.jsx
+++ part1/introduction-to-react/src/App.jsx
@@ -1,8 +1,15 @@
 const Hello = (props) => {
-  console.log(props)
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+  
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
@@ -13,11 +20,11 @@ const App = () => {
   const age = '10'
 
   return (
-    <>
+    <div>
       <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10}/>
+      <Hello name="Maya" age={26 + 10}/>
       <Hello name={name} age={age}/>
-    </>
+    </div>
   )
 }
 
