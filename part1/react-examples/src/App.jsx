8d56da6babe591ec0d60e34f767c7a3f674ea1a8
Paul Pham <paulpham1871@gmail.com>
2025-12-29 21:12:09 -0700

    Finished reading part 1c component state, event handlers

--- part1/react-examples/src/App.jsx
+++ part1/react-examples/src/App.jsx
@@ -1,45 +1,36 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>
 
-const Button = ({text, onClick}) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
+const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <Display
-        counter={counter}
-      />
-      <Button
-        text='plus'
-        onClick={increaseByOne}
-      />
-      <Button
-        text='zero'
-        onClick={setToZero}
-      />
-      <Button
-        text='minus'
-        onClick={decreaseByOne}
-      />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   )
-}
+} 
 
 export default App
