e68c0a4937c4ce43215fe658d1cc86e2a9400dfa
Paul Pham <paulpham1871@gmail.com>
2025-12-30 00:44:20 -0700

    Finished reading part 1d up to and including event handling revisited

--- part1/react-examples/src/App.jsx
+++ part1/react-examples/src/App.jsx
@@ -1,36 +1,19 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [value, setValue] = useState(10)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const handleClick = () => {
+    console.log('clicked the button')
+    setValue(0)
   }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {value}
+      <button onClick={handleClick}>button</button>
     </div>
   )
-} 
+}
 
 export default App
