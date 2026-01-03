c355e13baa424d955bbe00a0fe4fb29f3a85d354
Paul Pham <paulpham1871@gmail.com>
2026-01-02 22:42:42 -0700

    Completed exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,24 +2,39 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456'}
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addName = (event) => {
     event.preventDefault();
-    if (persons.findIndex(person => person.name === newName) !== -1) {
+
+    // Check fields
+    if (newName.length === 0 || newNumber.length === 0) {
+      alert(`Missing name or phone number`)
+      return
+    }
+
+    // Check if person already in there
+    const personToAdd = {name: newName, number: newNumber}
+    if (persons.findIndex(person => person.name === personToAdd.name) !== -1) {
       alert(`${newName} is already added to phonebook`);
-      setNewName('');
+      setNewName(``);
+      setNewNumber(``);
+      return
     }
-    else {
-      setPersons(persons.concat({name: newName}));
-      setNewName('');
-    }
+    setPersons(persons.concat(personToAdd));
+    setNewName(``);
+    setNewNumber(``);
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   }
 
   return (
@@ -33,13 +48,20 @@ const App = () => {
              onChange={handleNameChange}
           />
         </div>
+         <div>
+          number: 
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   );
