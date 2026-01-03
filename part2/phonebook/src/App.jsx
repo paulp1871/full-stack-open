7207c193023c036871f6803adb003695e0555391
Paul Pham <paulpham1871@gmail.com>
2026-01-02 23:36:46 -0700

    Completed exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const[newSearch, setNewSearch] = useState('');
 
   const addName = (event) => {
     event.preventDefault();
@@ -17,8 +21,8 @@ const App = () => {
     }
 
     // Check if person already in there
-    const personToAdd = {name: newName, number: newNumber}
-    if (persons.findIndex(person => person.name === personToAdd.name) !== -1) {
+    const personToAdd = {name: newName, number: newNumber, id: persons.length + 1}
+    if (persons.findIndex(person => person.name.toLowerCase() === personToAdd.name.toLowerCase()) !== -1) {
       alert(`${newName} is already added to phonebook`);
       setNewName(``);
       setNewNumber(``);
@@ -37,9 +41,29 @@ const App = () => {
     setNewNumber(event.target.value);
   }
 
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value);
+  }
+
+  const searchResult = (search) => {
+    if (search.length === 0) {
+      return persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)
+    }
+    const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+    return filteredPersons.map(person => <p key={person.id}>{person.name} {person.number}</p>)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+          value={newSearch}
+          onChange={handleSearchChange}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: 
@@ -60,9 +84,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
-      </ul>
+      {searchResult(newSearch)}
     </div>
   );
 }
