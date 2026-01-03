4d680f76349057de8d84f9113cb2ff3838ac2c1f
Paul Pham <paulpham1871@gmail.com>
2026-01-02 19:04:47 -0700

    Completed exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,30 +1,4 @@
-const Header = ({ name }) => <h2>{name}</h2>
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(
-      (part) => <Part key={part.id} name={part.name} exercises={part.exercises}/>
-    )}
-  </div>
-)
-
-const Part = ({ name, exercises }) => (
-  <p>
-    {name} {exercises}
-  </p>
-)
-
-const Total = ({ parts }) => <p><b>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
