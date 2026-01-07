2113824430ccb8d23b7ace9ea2cb662a03362aac
Paul Pham <paulpham1871@gmail.com>
2026-01-07 09:13:01 -0700

    Finished reading part 2e inline styles

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import Footer from './components/Footer'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -23,12 +25,11 @@ const App = () => {
     noteService
       .update(id, changedNote)
       .then(returnedNote => {
-        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
-        )
+        setErrorMessage(`Note '${note.content}' was already removed from server`)
+        setTimeout(() => {setErrorMessage(null)}, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -59,6 +60,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -75,7 +77,8 @@ const App = () => {
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form> 
+      </form>
+      <Footer /> 
     </div>
   )
 }
---
+++ part2/notes/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 15
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <p>
+        Note app, Department of Computer Science, University of Helsinki 2026
+      </p>
+    </div>
+  )
+}
+
+export default Footer
