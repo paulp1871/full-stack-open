f3f5c0e6a91d9c53116ded7a4854543a46c22a59
Paul Pham <paulpham1871@gmail.com>
2026-01-07 13:05:26 -0700

    Finished reading part 2

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -5,7 +5,7 @@ import noteService from './services/notes'
 import Notification from './components/Notification'
 
 const App = () => {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState(null)
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
@@ -17,6 +17,10 @@ const App = () => {
         setNotes(initialNotes)
       })
   }, [])
+
+  if (!notes) {
+    return null
+  }
 
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
@@ -56,7 +60,7 @@ const App = () => {
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
-
+  
   return (
     <div>
       <h1>Notes</h1>
