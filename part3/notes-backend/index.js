1e7d4253353546e982e9770f5dcba800e314e49b
Paul Pham <paulpham1871@gmail.com>
2026-01-29 00:20:42 -0700

    Finished reading part 3c a true full stack developer's oath

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,29 +1,10 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
+
 const app = express()
-const mongoose = require('mongoose')
 
-// mongodb database connections
-const password = process.argv[2]
-const url = `mongodb+srv://fso_notes_paulp1871:${password}@cluster0.t594rsh.mongodb.net/noteApp?appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url, { family: 4 })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-// turn model schema to proper json
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
+let notes = []
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -33,56 +14,41 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(requestLogger)
 app.use(express.static('dist'))
 app.use(express.json())
-app.use(requestLogger)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-// fetch notes from mongodb, not a hardcoded in-memory array now
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
+  Note.find({}).then((notes) => {
     response.json(notes)
   })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find((note) => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then((savedNote) => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -98,7 +64,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part3/notes-backend/mongo.js
+++ part3/notes-backend/mongo.js
@@ -7,45 +7,30 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fso_notes_paulp1871:${password}@cluster0.t594rsh.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
-// first argument is URL of database
-// second argument is object that defines required settings, tells us that we connect to database using IPv4 addresses
-mongoose.connect(url, { family: 4 })
-
-// tells Mongoose how the note objects are to be stored in the database (a blueprint at the app-level)
-// mongodb itself is schemaless, so we must enforce and validate our database rules at the app-level instead
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
 
-// models are classes/constructors created off the noteschema and their name
 const Note = mongoose.model('Note', noteSchema)
 
-// the model constructor creates a new JS object which also has access to all of the mongoose model's properties/methods
-const note = new Note({
-  content: 'Toji has so much frigging aura',
-  important: false,
-})
-
-// we save the created note model instance (a document) to our database with the save method provided by mongoose...
-// then once it is saved, an event handler is called which will log it as saved in the console and...
-// terminate the connection to the database
-// note.save().then(result => {
-//   // result is simply the response we get from mongodb once we save our document to the collection, usually the document itself
-//   console.log('note saved!', result)
-//   mongoose.connection.close()
+// const note = new Note({
+//   content: 'HTML is easy',
+//   important: true,
 // })
-// above code commented out to look at fetching objects from mongodb below
 
-// retrieve all documents from the database (more specifically, the notes collection) with the find method of the note model
-// the parameter of the method is an object expressing search conditions
-// search conditions must adhere to MongoDB's query syntax
-Note.find({important: false}).then(result => {
-  result.forEach(note => {
+// // note.save().then((result) => {
+// //   console.log('note saved!')
+// //   mongoose.connection.close()
+// // })
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
     console.log(note)
   })
   mongoose.connection.close()
